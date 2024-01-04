e15039915a051b7845ce59864e4270934c6d9cea
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-01-04 13:31:58 -0500

    init

---
+++ maelstrom-echo/test.sh
@@ -0,0 +1,2 @@
+go build .
+../maelstrom/maelstrom test -w echo --bin maelstrom-echo --node-count 1 --time-limit 10
---
+++ maelstrom-unique-ids/test.sh
@@ -0,0 +1,2 @@
+go build .
+../maelstrom/maelstrom test -w unique-ids --bin ./maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
